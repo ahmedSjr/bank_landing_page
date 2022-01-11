625b9eacd3870a5e0e40faabb4095218b74e5561
Ahemd Sirag <a7medsr.dev@gmail.com>
2022-01-11 23:52:23 +0200

    Implement page reval function

--- script.js
+++ script.js
@@ -149,6 +149,26 @@ const headerObs = new IntersectionObserver(stickyNav, {
 });
 headerObs.observe(header);
 
+// Reval sections using intersection observer
+
+const allSections = document.querySelectorAll('.section');
+const revelSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) return;
+  else entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+const sectionObs = new IntersectionObserver(revelSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObs.observe(section);
+  section.classList.add('section--hidden');
+});
+
 // const h1 = document.querySelector('h1');
 
 // console.log(h1.querySelectorAll('.highlight'));
