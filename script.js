1f424a862bc3ff16ca1b8fb43c0d9d7a1a08a598
Ahmed Sirag <58979522+ahmedSjr@users.noreply.github.com>
2022-01-16 06:46:14 +0200

    Slider version 1

--- script.js
+++ script.js
@@ -165,7 +165,7 @@ const sectionObs = new IntersectionObserver(revelSection, {
 
 allSections.forEach(function (section) {
   sectionObs.observe(section);
-  section.classList.add('section--hidden');
+  //   section.classList.add('section--hidden');
 });
 
 //Lazy Loading images
@@ -189,6 +189,46 @@ const imageObs = new IntersectionObserver(loading, {
 });
 
 allImages.forEach(img => imageObs.observe(img));
+
+//Apply The testimonial slider
+
+//Slider
+const slides = document.querySelectorAll('.slide');
+const slider = document.querySelector('.slider');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+let curSlide = 0;
+const maxSlide = slides.length;
+// slider.style.transform = `scale(0.4) translateX(-800px)`;
+// slider.style.overflow = `visible`;
+
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+  );
+};
+goToSlide(0);
+
+const nextSlide = function () {
+  if (curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  goToSlide(curSlide);
+};
+
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+  goToSlide(curSlide);
+};
+btnRight.addEventListener('click', nextSlide);
+btnLeft.addEventListener('click', prevSlide);
+
 // const h1 = document.querySelector('h1');
 
 // console.log(h1.querySelectorAll('.highlight'));
